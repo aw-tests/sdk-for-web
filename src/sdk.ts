7fc5d43bf51eafbfea88c16a6d8ba994e5557f49
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-11-24 12:36:54 +0100

    fix web

--- src/sdk.ts
+++ src/sdk.ts
@@ -3501,5 +3501,44 @@ class Appwrite {
 
 };
 
-export { Appwrite }
-export type { AppwriteException, Models }
+type QueryTypesSingle = string | number | boolean;
+type QueryTypesList = string[] | number[] | boolean[];
+type QueryTypes = QueryTypesSingle | QueryTypesList;
+
+class Query {
+  static equal = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "equal", value);
+
+  static notEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "notEqual", value);
+
+  static lesser = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "lesser", value);
+
+  static lesserEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "lesserEqual", value);
+
+  static greater = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "greater", value);
+
+  static greaterEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "greaterEqual", value);
+
+  static contains = (attribute: string, value: QueryTypesList): string =>
+    Query.addQuery(attribute, "contains", value);
+
+  private static addQuery = (attribute: string, oper: string, value: QueryTypes): string =>
+    value instanceof Array
+      ? `${attribute}.${oper}(${value
+          .map((v: QueryTypesSingle) => Query.parseValues(v))
+          .join(",")})`
+      : `${attribute}.${oper}(${Query.parseValues(value)})`;
+
+  private static parseValues = (value: QueryTypes): string =>
+    typeof value === "string" || value instanceof String
+      ? `"${value}"`
+      : `${value}`;
+}
+
+export { Appwrite, Query }
+export type { AppwriteException, Models, QueryTypes, QueryTypesList }
